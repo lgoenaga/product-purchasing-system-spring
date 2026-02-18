a8659f20a736513f463a398384fd17978c5dc16c
Luis Goenaga <lgoenaga@example.com>
2026-02-18 14:53:55 -0500

    refactor: migrate Address to Lombok annotations

--- src/main/java/co/edu/cesde/pps/model/Address.java
+++ src/main/java/co/edu/cesde/pps/model/Address.java
@@ -33,7 +33,6 @@ import java.util.Objects;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@ToString
 public class Address {
 
     private Long addressId;
@@ -45,22 +44,8 @@ public class Address {
     private String state;
     private String country;
     private String postalCode;
-    private Boolean isDefault;
-
-
-
-    // Constructor con campos obligatorios
-    public Address(User user, AddressType type, String line1, String city,
-                   String state, String country, String postalCode) {
-        this.user = user;
-        this.type = type;
-        this.line1 = line1;
-        this.city = city;
-        this.state = state;
-        this.country = country;
-        this.postalCode = postalCode;
-        this.isDefault = false;
-    }
+    @Builder.Default
+    private Boolean isDefault = false;
 
     // equals y hashCode basados en ID
 
@@ -77,4 +62,21 @@ public class Address {
         return Objects.hash(addressId);
     }
 
+    // toString personalizado sin navegación a objetos relacionados (solo IDs)
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "addressId=" + addressId +
+                ", userId=" + (user != null ? user.getUserId() : null) +
+                ", type=" + type +
+                ", line1='" + line1 + '\'' +
+                ", line2='" + line2 + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", country='" + country + '\'' +
+                ", postalCode='" + postalCode + '\'' +
+                ", isDefault=" + isDefault +
+                '}';
+    }
 }
